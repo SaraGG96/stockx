5cebc06647003c264b93f064818512f40c5b4474
Sara <saragarciagarrido96@gmail.com>
2026-05-13 14:14:55 +0200

    feat(Item):

--- app/src/main/java/edu/teamrocket/Item.java
+++ app/src/main/java/edu/teamrocket/Item.java
@@ -1,8 +1,15 @@
+package edu.teamrocket;
+import java.util.List;
+
 public interface Item {
-
     int getBid();
-
     int getAsk();
-
     int getSale();
+
+    void setBid(int bid);
+    void setAsk(int ask);
+    void setSale(int sale);
+
+    void add(Offer offer);
+    List<Offer> offers();
 }
