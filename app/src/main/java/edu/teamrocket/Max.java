489421398088318781eab76b1cac858c3532e6c8
Sara <saragarciagarrido96@gmail.com>
2026-05-18 12:49:03 +0200

    feat: Implementar el criterio "maximo"

---
+++ app/src/main/java/edu/teamrocket/Max.java
@@ -0,0 +1,25 @@
+package edu.teamrocket;
+import java.util.List;
+
+public class Max implements Criteria {
+
+    private Criteria criteria;
+    private Criteria otherCriteria;
+
+    public Max(Criteria criteria, Criteria otherCriteria) {
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+
+    public List<Offer> checkCriteria(Item item) {
+        List<Offer> offers = criteria.checkCriteria(item);
+        List<Offer> otherOffers = otherCriteria.checkCriteria(item);
+        offers.addAll(otherOffers);
+        return offers.stream()
+                .max(Offer::compareTo)
+                .stream()
+                .toList();
+    }
+
+    
+}
